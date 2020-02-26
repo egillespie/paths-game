b140631fbd2e64b69915204beb4d82cfda9aa369
Erik Gillespie <erik.gillespie@gmail.com>
2020-02-25 22:29:12 -0500

    SVG icons for showing file tree

--- components/file.vue
+++ components/file.vue
@@ -1,9 +1,9 @@
 <template>
-  <li class="file">
-    <span v-if="children">📂</span>
-    <span v-else>🗒️</span>
+  <li class="file leading-5">
+    <img v-if="children" src="@/assets/svg/folder-icon.svg" class="inline h-5">
+    <img v-else src="@/assets/svg/file-icon.svg" class="inline h-5">
     <code>{{ name }}</code>
-    <file-tree v-if="children" :files="children" />
+    <file-tree v-if="children" :files="children" class="file-tree" />
   </li>
 </template>
 
@@ -30,8 +30,11 @@ export default {
 
 <style>
 .file .file {
+  margin-left: .8rem;
+}
+
+.file-tree {
   border-left: 1px dotted lightgray;
-  margin-left: .5rem;
-  padding-left: 1rem;
+  margin-left: .625rem;
 }
 </style>
