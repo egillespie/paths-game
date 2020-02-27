9b8d1d0db8e6f54b33451492732112914562be7e
Erik Gillespie <erik.gillespie@gmail.com>
2020-02-27 16:55:35 -0500

    Show different icon for image files

--- components/file.vue
+++ components/file.vue
@@ -2,6 +2,7 @@
   <li class="file leading-5">
     <span class="inline-block p-1 border border-transparent border-solid hover:border-blue-500 hover:cursor-pointer">
       <img v-if="children" src="@/assets/svg/folder-icon.svg" class="inline h-6 -mt-1">
+      <img v-else-if="isImage" src="@/assets/svg/image-icon.svg" class="inline h-6 -mt-1">
       <img v-else src="@/assets/svg/file-icon.svg" class="inline h-6 -mt-1">
       <code class="inline">{{ name }}</code>
     </span>
@@ -25,6 +26,12 @@ export default {
       type: Object,
       required: false,
       default: null
+    }
+  },
+  computed: {
+    isImage () {
+      const ext = this.name.split('.').pop()
+      return ['gif', 'jpg', 'jpeg', 'png', 'svg'].includes(ext)
     }
   }
 }
